bc9a2656fe55c7e1ec40c56ae8fd557b8e6781a6
xaukael <37848032+xaukael@users.noreply.github.com>
2023-07-13 10:44:12 -0500

    Add files via upload

--- pointers.js
+++ pointers.js
@@ -6,23 +6,20 @@ Hooks.once("socketlib.ready", () => {
   function point(userId, show, x, y, icon) {
    let user = game.users.get(userId)
    if (user.viewedScene!=canvas.scene.id) return;
-   let size = canvas.grid.size;
+   let size = canvas.grid.size*game.settings.get('pointers', 'sizeMultiplier');
    let $bubble = $(`#${userId}-talk-bubble`);
    if (!show) return $bubble.remove();
    if ($bubble.length) return $bubble.css({top: `${y-size}px`, left: `${x}px`})
    $bubble = $(`<div id="${userId}-talk-bubble" style="position: absolute;  top: ${y-size}px; left: ${x}px; ">
     <style>
     .talk-icon {
-      color: var(--color-text-light-2);
-      cursor: none;
-      pointer-events: all !important;
       font-size: ${size}px;
-      text-shadow: 0 0 8px #000000;
-      opacity: .9;
+      ${game.settings.get('pointers', 'css')}
+      color:${user.color};
     }
     #hud {pointer-events: all !important;}
     </style>
-    <span class="talk-icon" style="color:${user.color}"><i class="fas ${icon}"></i></span>
+    <span class="talk-icon" style="">${icon}</span>
     </div>`);
 
     return  $(`#hud`).append($bubble);
@@ -44,12 +41,65 @@ Hooks.once("socketlib.ready", () => {
  }
 });
 
+Hooks.on('renderSettingsConfig', (app, html)=>{
+ let faLink = $(`<a href="https://fontawesome.com/search?o=r&m=free" target="_blank">Find Icons</a>`);
+ html.find('section[data-tab=pointers] p.notes:contains("HTML")').append(faLink)
+});
+
 Hooks.once('init', () => {
+  game.settings.register('pointers', 'css', {
+    name: `CSS`,
+    hint: `CSS Style to apply to pointers`,
+    scope: "world",
+    type: String,
+    default: "text-shadow: 0 0 8px #000000; opacity: .9;",
+    config: true  
+ });
+ game.settings.register('pointers', 'sizeMultiplier', {
+    name: `Grid Size Multiplier`,
+    hint: `Scale of pointer to grid size`,
+    scope: "world",
+    type: Number,
+    default: 1,
+    config: true  
+ });
+ game.settings.register('pointers', 'arrow', {
+    name: `Arrow`,
+    hint: `HTML for the arrow pointer `,
+    scope: "world",
+    type: String,
+    default: `<i class="fas fa-location-arrow fa-rotate-180"></i>`,
+    config: true  
+ });
+ game.settings.register('pointers', 'speak', {
+    name: `Speak`,
+    hint: `HTML for the speak pointer `,
+    scope: "world",
+    type: String,
+    default: `<i class="fas fa-comment"></i>`,
+    config: true  
+ });
+ game.settings.register('pointers', 'think', {
+    name: `Think`,
+    hint: `HTML for the think pointer `,
+    scope: "world",
+    type: String,
+    default: `<i class="fas fa-lightbulb"></i>`,
+    config: true  
+ });
+ game.settings.register('pointers', 'point', {
+    name: `Point`,
+    hint: `HTML for the point pointer `,
+    scope: "world",
+    type: String,
+    default: `<i class="fas fa-hand-point-down"></i>`,
+    config: true  
+ });
  game.keybindings.register("pointers", "showArrow", {
      name: "Arrow",
      hint: "Show Arrow at cursor",
      editable: [{key: "KeyX"}],
-     onDown: (e) => { return pointerSocket.emit(true, "fa-location-arrow fa-rotate-180") },
+     onDown: (e) => { return pointerSocket.emit(true, game.settings.get('pointers', 'arrow') ) },
      onUp: () => { return pointerSocket.emit(false) },      
      precedence: CONST.KEYBINDING_PRECEDENCE.PRIORITY
  });
@@ -57,7 +107,7 @@ Hooks.once('init', () => {
      name: "Speak",
      hint: "Show speach bubble at cursor",
      editable: [{key: "KeyX", modifiers: [KeyboardManager.MODIFIER_KEYS.CONTROL] }],
-     onDown: () => { return pointerSocket.emit(true, "fa-comment") },
+     onDown: () => { return pointerSocket.emit(true, game.settings.get('pointers', 'speak') ) },
      onUp: () => { return pointerSocket.emit(false, "") },      
      precedence: CONST.KEYBINDING_PRECEDENCE.PRIORITY
  });
@@ -65,7 +115,7 @@ Hooks.once('init', () => {
      name: "Think",
      hint: "Show light bulb at cursor",
      editable: [{key: "KeyX", modifiers: [KeyboardManager.MODIFIER_KEYS.SHIFT] }],
-     onDown: (e) => { return pointerSocket.emit(true, "fa-lightbulb") },
+     onDown: (e) => { return pointerSocket.emit(true, game.settings.get('pointers', 'think') ) },
      onUp: () => { return pointerSocket.emit(false) },      
      precedence: CONST.KEYBINDING_PRECEDENCE.PRIORITY
  });
@@ -73,8 +123,9 @@ Hooks.once('init', () => {
      name: "Point",
      hint: "Show pointer at cursor",
      editable: [{key: "KeyX", modifiers: [KeyboardManager.MODIFIER_KEYS.SHIFT, KeyboardManager.MODIFIER_KEYS.CONTROL] }],
-     onDown: (e) => { return pointerSocket.emit(true, "fa-hand-point-down") },
+     onDown: (e) => { return pointerSocket.emit(true, game.settings.get('pointers', 'point') ) },
      onUp: () => { return pointerSocket.emit(false) },      
      precedence: CONST.KEYBINDING_PRECEDENCE.PRIORITY
  });
+ 
 });
